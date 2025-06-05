7729ff147fe7f6309a7ba0753e6e809b4b165bab
arkyan <julien.gardin@live.fr>
2025-06-05 14:36:56 +0200

    Init projects

---
+++ poulet-mecanique-backend/src/main/java/fr/arkyan/popo/pouletmecaniquebackend/PouletMecaniqueBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.arkyan.popo.pouletmecaniquebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PouletMecaniqueBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PouletMecaniqueBackendApplication.class, args);
+	}
+
+}
---
+++ poulet-mecanique-backend/src/test/java/fr/arkyan/popo/pouletmecaniquebackend/PouletMecaniqueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.arkyan.popo.pouletmecaniquebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PouletMecaniqueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
